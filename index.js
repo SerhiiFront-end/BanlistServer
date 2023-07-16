dcfb59c0e1eab9c1e1ee431cfaeee3ba3535bba0
Serhii <aorl9048@gmail.com>
2023-07-16 02:39:08 +0100

    s

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function decodeUnicodeEscapes(text) {
 }
 async function banlistLoad(server) {
 	const browser = await puppeteer.launch({ headless: 'new' })
-	const page = await browser.newPage()
+	const page = await browser.newPage() //
 	await page.goto(
 		`https://gta-trinity.com/forum/api/monitoring/?csrfKey=ed2e21b61c22a21e55e89e427ba4eafc&draw=21&columns%5B0%5D%5Bdata%5D=0&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=1&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=desc&start=0&length=6300&search%5Bvalue%5D=&search%5Bregex%5D=false&server=1&monitoring=${server}ban&_=1683791465568`
 	)
