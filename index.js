338a1aef1396eba603748a31ed5a78958723ad78
Serhii <aorl9048@gmail.com>
2024-03-06 03:20:00 +0000

    commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const port = process.env.PORT || 3000
 const app = express()
-const banlists = require('./updatings')
 app.use(express.json())
 
 app.get('/', async (req, res) => {
 	res.send('go to /banlist/web/ ( 1rp / 2rp / rpg )')
 })
 app.get('/banlist/web/:server', async (req, res) => {
+	const banlists = require('./updatings')
 	res.json(banlists[req.params.server])
 })
 app.listen(port, () => {
