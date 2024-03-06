0526bff43221ae88c31a3a0dc4f742d2ba948baf
Serhii <aorl9048@gmail.com>
2024-03-06 02:21:36 +0000

    commit

--- index.js
+++ index.js
@@ -49,25 +49,52 @@ async function banlistLoad(server) {
 		// )
 	}, 6000)
 }
-cron.schedule(
-	'* * * * *',
-	() => {
-		banlistLoad('1rp')
-		banlistLoad('2rp')
-		banlistLoad('rpg')
-	},
-	{
-		scheduled: true,
-		timezone: 'Europe/Moscow',
-	}
-)
+// cron.schedule(
+// 	'* * * * *',
+// 	() => {
+// 		banlistLoad('1rp')
+// 		banlistLoad('2rp')
+// 		banlistLoad('rpg')
+// 	},
+// 	{
+// 		scheduled: true,
+// 		timezone: 'Europe/Moscow',
+// 	}
+// )
 
 app.get('/', async (req, res) => {
 	res.send('go to /banlist/web/ ( 1rp / 2rp / rpg )')
 })
 app.get('/banlist/web/:server', async (req, res) => {
 	// const banlist = require(`./public/banlists/${req.params.server}banlist.json`)
-	res.json(fullBans[req.params.server])
+	// res.json(banlistLoad(req.params.server))
+	const browser = await puppeteer.launch({ headless: 'new' })
+	const page = await browser.newPage() //
+	await page.goto(
+		`https://gta-trinity.com/forum/api/monitoring/?csrfKey=ed2e21b61c22a21e55e89e427ba4eafc&draw=21&columns%5B0%5D%5Bdata%5D=0&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=1&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=desc&start=0&length=6300&search%5Bvalue%5D=&search%5Bregex%5D=false&server=1&monitoring=${req.params.server}ban&_=1683791465568`
+	)
+
+	setTimeout(async () => {
+		const content = await page.content()
+		const decodedText = decodeUnicodeEscapes(content)
+		const matches = decodedText.match(/\[(\d{2}:\d{2}:\d{4})\].*?\..*?/g)
+		const result = matches.map(match => match.slice(0, -1) + '.')
+		await browser.close()
+		// fullBans[server] = JSON.stringify(result)
+		fullBans[req.params.server] = result
+		// fs.writeFile(
+		// 	`./public/banlists/${server}banlist.json`,
+		// 	JSON.stringify(result),
+		// 	err => {
+		// 		if (err) {
+		// 			console.error('Ошибка при записи файла:', err)
+		// 		} else {
+		// 			console.log(`Запись в файл успешно выполнена, сервер: ${server}.`)
+		// 		}
+		// 	}
+		// )
+		res.json(result)
+	}, 6000)
 })
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`)
